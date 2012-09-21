e9fb784a3ed95b2f7977d043af2cced77931f83a
Rafael Caricio <rafael.jacinto@gmail.com>
2012-09-21 16:18:24 -0300

    add a utility method to test get requests.

--- tests/test_healthcheck.py
+++ tests/test_healthcheck.py
@@ -8,3 +8,8 @@ class TestHealthCheckHandlerTestCase(BaseImagesAPITestCase):
     def test_access_to_healthcheck(self):
         response = self.get('/healthcheck')
         self.assertEqual(response.body, "WORKING")
+
+    def test_use_of_querystring(self):
+        response = self.get('/healthcheck', my_data=1)
+        self.assertTrue(
+                response.effective_url.endswith("/healthcheck?my_data=1"))
