c39837fdf22f05f577c0de4b274cd05df47d6c44
Rafael Caricio <rafael.jacinto@gmail.com>
2012-09-21 16:04:41 -0300

    moving behaviour to be easier to write tests.

--- tests/test_healthcheck.py
+++ tests/test_healthcheck.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from tornado.testing import AsyncHTTPTestCase
+from tests import BaseImagesAPITestCase
 
-from images_api.app import ImagesApplication
-
-
-class TestHealthCheckHandlerTestCase(AsyncHTTPTestCase):
-    def get_app(self):
-        return ImagesApplication()
+class TestHealthCheckHandlerTestCase(BaseImagesAPITestCase):
 
     def test_access_to_healthcheck(self):
-        self.http_client.fetch(self.get_url('/healthcheck'), self.stop)
-        response = self.wait()
+        response = self.get('/healthcheck')
         self.assertEqual(response.body, "WORKING")
