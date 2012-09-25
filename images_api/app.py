68982b55c2f7adf47c45c0b2231d56b8f9dbe796
Rafael Caricio <rafael.jacinto@gmail.com>
2012-09-25 15:54:40 -0300

    add handler to generate thumbor urls.

--- images_api/app.py
+++ images_api/app.py
@@ -4,6 +4,8 @@
 import tornado.web
 from tornado import httpclient
 
+from tornado_thumbor_url.handlers import GenerateThumborUrlHandler
+
 from images_api.handlers import HealthCheckHandler
 from images_api.alpha.handlers import SearchHandler
 
@@ -14,6 +16,9 @@ class ImagesApplication(tornado.web.Application):
                 'tornado.curl_httpclient.CurlAsyncHTTPClient')
         handlers = [
             (r'/healthcheck(?:/|\.html)?', HealthCheckHandler),
-            (r'/alpha/search', SearchHandler),
+            (r'/alpha/search/?', SearchHandler),
+            (r'/thumbor_urls/?', GenerateThumborUrlHandler),
         ]
-        super(ImagesApplication, self).__init__(handlers)
+        super(ImagesApplication, self).__init__(handlers,
+                thumbor_security_key='abc',
+                thumbor_server_url='http://localhost:8888/')
