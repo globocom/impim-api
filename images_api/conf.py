3bf3a9d21573723384f00f1e8b6778829ad13044
Rafael Caricio <rafael.jacinto@gmail.com>
2012-09-27 16:29:47 -0300

    update elastic search default url for search.

--- images_api/conf.py
+++ images_api/conf.py
@@ -5,7 +5,7 @@ from derpconf.config import Config
 
 Config.define('JSONP_CALLBACK', 'defaultCallback', 'Callback default for JSONP responses.', 'General')
 
-Config.define('ELASTIC_SEARCH_PHOTOS_URL', 'http://localhost:9200/images-api/image/_search', 'ElasticSearch url to search for photos.', 'Photo Search')
+Config.define('ELASTIC_SEARCH_PHOTOS_URL', 'http://esearch.dev.globoi.com/images/image/_search', 'ElasticSearch url to search for photos.', 'Photo Search')
 
 Config.define('THUMBOR_SECURITY_KEY', 'abc', 'The thumbor security key.', 'Thumbor configuration')
 Config.define('THUMBOR_SERVER_URL', 'http://localhost:8888/', 'The thumbor server url.', 'Thumbor configuration')
