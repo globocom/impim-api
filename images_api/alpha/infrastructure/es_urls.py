db0d948ee2a2ad107aface03f182ae5ecd08d3f0
Guilherme Cirne <gcirne@gmail.com>
2012-09-28 15:33:55 -0300

    adds type_url to EsUrls.

--- images_api/alpha/infrastructure/es_urls.py
+++ images_api/alpha/infrastructure/es_urls.py
@@ -6,6 +6,10 @@ class EsUrls(object):
     IMAGE_TYPE = 'image'
     
     @classmethod
+    def type_url(cls, type):
+        return 'http://esearch.dev.globoi.com/images/%s' % type
+    
+    @classmethod
     def search_url(cls, type):
-        return 'http://esearch.dev.globoi.com/images/%s/_search' % type
+        return cls.type_url(type) + '/_search'
         
--- tests/unit/alpha/infrastructure/test_es_urls.py
+++ tests/unit/alpha/infrastructure/test_es_urls.py
@@ -7,5 +7,8 @@ from images_api.alpha.infrastructure import EsUrls
 
 class EsParserTestCase(TestCase):
     
+    def test_type_url(self):
+        assert EsUrls.type_url(type='image') == 'http://esearch.dev.globoi.com/images/image'
+    
     def test_search_url(self):
         assert EsUrls.search_url(type='image') == 'http://esearch.dev.globoi.com/images/image/_search'
