dbcd09dc2b7273f159c9e52ef5ea503d3fe19ffd
Rafael Caricio <rafael.jacinto@gmail.com>
2012-10-01 16:52:17 -0300

    move bahaviour to the alpha release.

--- images_api/alpha/handlers/base.py
+++ images_api/alpha/handlers/base.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from json import dumps
+
 from images_api.handlers.base import BaseHandler
 
 
 class AlphaBaseHandler(BaseHandler):
-    pass
+
+    def _error(self, status, msg=None):
+        self.set_status(status)
+        self.finish()
+
+    def respond_with(self, data):
+        self.set_header('Content-Type', 'application/json')
+        data = dumps(data)
+        callback_name = self.get_argument('callback', default=None)
+        if callback_name:
+            data = "%s(%s)" % (callback_name, data)
+        self.write(data)
+        self.finish()
+
+
