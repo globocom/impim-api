5455e0ef1911c05959e887081191f3c8a79aa395
Guilherme Cirne <gcirne@gmail.com>
2012-10-08 16:21:25 -0300

    refactoring tests.

--- tests/acceptance/test_search.py
+++ tests/acceptance/test_search.py
@@ -4,21 +4,29 @@
 
 from json import dumps, loads
 
+from images_api.infrastructure.elastic_search import Urls
+
+from tests.support import es_cleanup
 from tests.support import ImagesAPIAsyncHTTPTestCase
+from tests.support import MockConfig
 
 
 class ImageSearchTestCase(ImagesAPIAsyncHTTPTestCase):
 
     def setUp(self):
         super(ImageSearchTestCase, self).setUp()
+        self._elastic_search_urls = Urls(MockConfig())
+        es_cleanup(self._elastic_search_urls)
+        self.post(self._elastic_search_urls.type_url(Urls.IMAGE_TYPE), dumps({'title': u'Title'}))
+        self.post(self._elastic_search_urls.refresh_url(), '')
 
-    def test_search_without_filters(self):
+    def test_search(self):
         response = self.get('/alpha/search')
 
-        self.assertEqual(response.code, 200)
-        self.assertTrue('application/json' in response.headers['Content-Type'])
-        results = loads(response.body)
-        self.assertTrue('items' in results)
+        assert response.code == 200
+        assert 'application/json' in response.headers['Content-Type']
+        body = loads(response.body)
+        assert body == {u'items': [{u'title': u'Title'}], u'total': 1, u'pageSize': 10}
 
     def test_search_with_callback(self):
         response = self.get('/alpha/search', callback='my_images')
