60d6920785b72594d47fabf286a49e9865bcda96
Guilherme Cirne <gcirne@gmail.com>
2012-10-09 17:54:16 -0300

    MockConfig populated from images_api.test.conf file.

--- tests/support/mock_config.py
+++ tests/support/mock_config.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
+from os.path import abspath, dirname, join
+
+from images_api.conf import Config
+
+
 class MockConfig(object):
 
-    ELASTIC_SEARCH_BASE_URL = 'http://localhost:9200'
-    ELASTIC_SEARCH_INDEX = 'images-test'
+    config = Config.load(path=abspath(join(dirname(__file__), '..', '..', 'images_api.test.conf')), conf_name='images_api.test.conf')
+    ELASTIC_SEARCH_BASE_URL = config.ELASTIC_SEARCH_BASE_URL
+    ELASTIC_SEARCH_INDEX = config.ELASTIC_SEARCH_INDEX
