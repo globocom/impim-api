e707714d359dc5a4b049e007949dc86f52ada4b1
Rafael Caricio <rafael.jacinto@gmail.com>
2012-10-11 16:01:21 -0300

    moving restiful api unit tests to propper directory

---
+++ tests/unit/rest_api/test_restiful_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from unittest import TestCase
+
+from images_api.rest_api import TornadoRESTful, ResourceHandler
+
+
+class TestApiManager(TestCase):
+
+    def setUp(self):
+        self.api = TornadoRESTful()
+
+    def test_should_be_possible_to_add_a_handler(self):
+        self.api.add_resource('api', ResourceHandler)
+        assert (r'/api/?', ResourceHandler) in self.api.get_url_mapping()
+
+    def test_should_generate_a_path_for_access_direcly_an_instance(self):
+        self.api.add_resource('comment', ResourceHandler)
+        assert (r'/comment/(.+)/?', ResourceHandler) in \
+                self.api.get_url_mapping()
