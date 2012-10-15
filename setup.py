69a1813efd72136b7e609b41f60ad1d1492a2d28
Guilherme Cirne <gcirne@gmail.com>
2012-10-15 13:29:13 -0300

    initial setup.py.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='images-api',
+    version='0.1.0dev',
+    author='globo.com',
+    author_email='lambda@corp.globo.com'
+)
