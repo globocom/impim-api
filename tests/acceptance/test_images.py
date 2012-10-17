1c8f05b33e610efb44d9537890498673dfe440a3
Guilherme Cirne <gcirne@gmail.com>
2012-10-17 19:02:41 -0300

    renaming test.

--- tests/acceptance/test_images.py
+++ tests/acceptance/test_images.py
@@ -13,10 +13,10 @@ from tests.support import ImagesAPIAsyncHTTPTestCase
 from tests.support import MockConfig
 
 
-class ImageSearchTestCase(ImagesAPIAsyncHTTPTestCase, ElasticSearchMixin):
+class ImagesTestCase(ImagesAPIAsyncHTTPTestCase, ElasticSearchMixin):
 
     def setUp(self):
-        super(ImageSearchTestCase, self).setUp()
+        super(ImagesTestCase, self).setUp()
         self._elastic_search_urls = Urls(MockConfig())
         es_cleanup(self._elastic_search_urls)
         self.post_to_elastic_search(self._elastic_search_urls.type_url(Urls.IMAGE_TYPE), {
@@ -25,7 +25,7 @@ class ImageSearchTestCase(ImagesAPIAsyncHTTPTestCase, ElasticSearchMixin):
             'eventDate': '2012-10-08T17:02:00',
         })
 
-    def test_search(self):
+    def test_images(self):
         response = self.get('/alpha/images')
 
         assert response.code == 200, 'the response code should be 200 but it ' \
@@ -34,7 +34,7 @@ class ImageSearchTestCase(ImagesAPIAsyncHTTPTestCase, ElasticSearchMixin):
         body = loads(response.body)
         assert body == {u'items': [{u'title': u'Title', u'eventDate': u'2012-10-08T17:02:00', u'createdDate': u'2012-10-08T17:02:00'}], u'total': 1, u'pageSize': 10}
 
-    def test_search_with_query_string(self):
+    def test_images_with_query_string(self):
         query_string = {
             'q': 'Title',
             'createdDateFrom': '2012-10-08T17:02:00',
@@ -51,7 +51,7 @@ class ImageSearchTestCase(ImagesAPIAsyncHTTPTestCase, ElasticSearchMixin):
         body = loads(response.body)
         assert body == {u'items': [{u'title': u'Title', u'eventDate': u'2012-10-08T17:02:00', u'createdDate': u'2012-10-08T17:02:00'}], u'total': 1, u'pageSize': 10}
 
-    def test_search_with_empty_query_string(self):
+    def test_images_with_empty_query_string(self):
         response = self.get('/alpha/images?q=&createdDateFrom=&createdDateTo=&eventDateFrom=&eventDateTo=&page=&pageSize=')
 
         assert response.code == 200
@@ -59,7 +59,7 @@ class ImageSearchTestCase(ImagesAPIAsyncHTTPTestCase, ElasticSearchMixin):
         body = loads(response.body)
         assert body == {u'items': [{u'title': u'Title', u'eventDate': u'2012-10-08T17:02:00', u'createdDate': u'2012-10-08T17:02:00'}], u'total': 1, u'pageSize': 10}
 
-    def test_search_with_callback(self):
+    def test_images_with_callback(self):
         response = self._fetch(
             self.get_url('/alpha/images?callback=my_images'), 'GET', headers={
                 'Accept': 'text/javascript'
