f539e9356266c04719fab8f74f245be015550704
Guilherme Cirne <gcirne@gmail.com>
2012-10-18 13:04:43 -0300

    No more camel case API params.

--- tests/acceptance/test_images.py
+++ tests/acceptance/test_images.py
@@ -37,12 +37,12 @@ class ImagesTestCase(ImagesAPIAsyncHTTPTestCase, ElasticSearchMixin):
     def test_images_with_query_string(self):
         query_string = {
             'q': 'Title',
-            'createdDateFrom': '2012-10-08T17:02:00',
-            'createdDateTo': '2012-10-08T17:02:00',
-            'eventDateFrom': '2012-10-08T17:02:00',
-            'eventDateTo': '2012-10-08T17:02:00',
+            'created_date_from': '2012-10-08T17:02:00',
+            'created_date_to': '2012-10-08T17:02:00',
+            'event_date_from': '2012-10-08T17:02:00',
+            'event_date_to': '2012-10-08T17:02:00',
             'page': '1',
-            'pageSize': '10',
+            'page_size': '10',
         }
         response = self.get('/alpha/images?%s' % urlencode(query_string))
 
@@ -52,7 +52,7 @@ class ImagesTestCase(ImagesAPIAsyncHTTPTestCase, ElasticSearchMixin):
         assert body == {u'items': [{u'title': u'Title', u'eventDate': u'2012-10-08T17:02:00', u'createdDate': u'2012-10-08T17:02:00'}], u'total': 1, u'pageSize': 10}
 
     def test_images_with_empty_query_string(self):
-        response = self.get('/alpha/images?q=&createdDateFrom=&createdDateTo=&eventDateFrom=&eventDateTo=&page=&pageSize=')
+        response = self.get('/alpha/images?q=&created_date_from=&created_date_to=&event_date_from=&event_date_to=&page=&page_size=')
 
         assert response.code == 200
         assert 'application/json' in response.headers['Content-Type']
