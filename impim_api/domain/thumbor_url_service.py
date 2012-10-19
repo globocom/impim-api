b35d097a9c609530e7d706a8a304bc083b416aa5
Guilherme Cirne <gcirne@gmail.com>
2012-10-19 17:48:24 -0300

    trying to fix build.

--- impim_api/domain/thumbor_url_service.py
+++ impim_api/domain/thumbor_url_service.py
@@ -13,10 +13,8 @@ class ThumborUrlService(object):
     # TODO: get thumbor security key from config.
     def fit_in_urls(self, original_url, sizes):
         crypto = CryptoURL(key='MY_SECURE_KEY')
-        urls = []
+        urls = {}
         for size in sizes:
             split_size = size.split('x')
-            urls.append({
-                size: crypto.generate(image_url=original_url, width=split_size[0], height=split_size[1], fit_in=True)
-            })
+            urls[size] = crypto.generate(image_url=original_url, width=split_size[0], height=split_size[1], fit_in=True)
         return urls
