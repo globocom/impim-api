5c8020dc46da6e78736815f8de1c466c80afa8ea
Guilherme Cirne <gcirne@gmail.com>
2012-10-26 13:43:04 -0200

    removing unused method.

--- tests/support/elastic_search_mixin.py
+++ tests/support/elastic_search_mixin.py
@@ -6,10 +6,6 @@ from json import dumps
 
 
 class ElasticSearchMixin(object):
-    
-    def post_to_elastic_search(self, url, data):
-        self.post(url, dumps(data))
-        self.refresh_elastic_search()
 
     def refresh_elastic_search(self):
         self.post(self._elastic_search_urls.refresh_url(), '')
