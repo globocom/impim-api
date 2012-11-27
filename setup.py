e2a355f7fb4c4573e23a7a0780dbbf3af00e6a61
Rafael Caricio <rafael.jacinto@gmail.com>
2012-11-27 16:55:50 -0200

    version 0.1.5

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ def readfile(file_name):
 
 setup(
     name='impim-api',
-    version='0.1.3',
+    version='0.1.5',
     author='globo.com',
     author_email='lambda@corp.globo.com',
     license = 'MIT',
     url="https://github.com/globocom/impim-api",
     include_package_data=True,
-    packages = find_packages('impim_api'),
+    packages = find_packages(exclude=["tests.*", "tests"]),
     package_dir = {"impim_api": "impim_api"},
     install_requires=[requirement for requirement in readfile('requirements.txt').split('\n') if requirement],
     entry_points = {
