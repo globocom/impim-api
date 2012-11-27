e1419dc7f025bae3c323909fc614219979d92174
Rafael Caricio <rafael.jacinto@gmail.com>
2012-11-27 16:21:25 -0200

    version 0.1.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def readfile(file_name):
 
 setup(
     name='impim-api',
-    version='0.1.0',
+    version='0.1.1',
     author='globo.com',
     author_email='lambda@corp.globo.com',
     license = 'MIT',
