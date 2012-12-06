294426b47583043f7dd2a6275b4eab63fdcdcda6
Guilherme Cirne <gcirne@gmail.com>
2012-12-06 16:25:39 -0200

    adds AWS_BUCKET_NAME to configuration.

--- impim_api/conf.py
+++ impim_api/conf.py
@@ -18,11 +18,12 @@ Config.define('ELASTIC_SEARCH_INDEX', 'impim', 'ElasticSearch index.', 'ElasticS
 
 Config.define('FILE_SYSTEM_ROOT_PATH', '/tmp/impim-api', 'File System root path.', 'File System Storage')
 
+Config.define('AWS_ACCESS_KEY_ID', 'xxx', 'The amazon access key.', 'AWS')
+Config.define('AWS_SECRET_ACCESS_KEY', 'yyy', 'The amazon secret key.', 'AWS')
+Config.define('AWS_BUCKET_NAME', 'my_bucket', 'The S3 bucket name.', 'AWS')
+
 Config.define('THUMBOR_SECURITY_KEY', 'MY_SECURE_KEY', 'Thumbor security key.', 'Thumbor configuration')
 Config.define('THUMBOR_SERVER_URL', 'http://localhost:8888/', 'Thumbor server url.', 'Thumbor configuration')
-
-Config.define('AWS_ACCESS_KEY_ID', 'xxx', 'The amazon access key.', 'S3 Storage')
-Config.define('AWS_SECRET_ACCESS_KEY', 'yyy', 'The amazon secret key.', 'S3 Storage')
 
 
 def generate_config():
